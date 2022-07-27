dfe9665a5d9b54ce807ebd01041e9fa8b9287b79
Aniket Ghute <aniket.ghute@noovosoft.com>
2022-07-27 16:43:09 +0530

    added cart and custom hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,26 @@
 import React from 'react';
-// import logo from './logo.svg';
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import logo from './logo.svg';
 import './App.css';
-import Home from "./components/home";
-import UserCart from "./components/UserCart";
-import ProductList from "./components/ProductList";
-import {UserProfile} from "./components/UserProfile";
-import {Store} from "./store";
-
 
 function App() {
-    return (
-        <>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<Home store={Store}/>}>
-                        <Route index element={<ProductList store={Store}/>}/>
-                        <Route path="userCart/2" element={<UserCart store={Store}/>}/>
-                        <Route path="myProfile/2" element={<UserProfile id={1} store={Store}/>}/>
-                    </Route>
-                </Routes>
-            </BrowserRouter>
-        </>
-    )
-        ;
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,84 +1,64 @@
-import React, {FunctionComponent, useEffect} from 'react';
-import '../styles/home.css'
+import React, {FunctionComponent, useEffect, useState} from 'react';
+import {Button, Card, CardBody, CardGroup, CardImg, CardSubtitle, CardText, CardTitle} from "reactstrap";
 
-import {Outlet} from "react-router-dom";
-import {observer} from "mobx-react";
-import {Toast, ToastBody, ToastHeader} from "reactstrap";
+//
+// interface OwnProps {
+//     data: any
+// }
 
-const Home: FunctionComponent<any> = observer((props) => {
-    const {categories} = props?.store?.allCategories
-    const {data} = props?.store?.products
+// type Props = OwnProps;
 
-
+const Home: FunctionComponent<any> = () => {
+    const [data, setData] = useState<any>();
+    const getData = async () => {
+        const res = await fetch('https://dummyjson.com/products');
+        const response = await res.json();
+        console.log(response);
+        setData(response.products);
+    }
     useEffect(() => {
-        props.store.getCategoriesData()
-        props.store.getDisplayedProducts()
-    }, [props.store]);
-
-    const searchProduct = (e: React.ChangeEvent<HTMLInputElement>) => {
-        props.store.searchProducts(e.target.value)
-    }
-    const handleCategorySelection = (category: string) => {
-        props.store.getProductsFromSelectedCategory(category)
-    }
-
+        getData();
+    }, []);
     return (
         <>
-            <section className="page">
-                <header>
-                    <nav className="navbar navbar-dark bg-dark w-100 pe-5">
-                        <a className="navbar-brand ms-5">Noovosoft Poc</a>
-                        <form className="form-inline">
-                            <input className="form-control me-5" onChange={(e) => searchProduct(e)} type="search"
-                                   placeholder="Search" aria-label="Search"/>
-                        </form>
-                    </nav>
-
-                </header>
-                <nav>
-                    <div className="wrapper h-100 w-100 pt-4 ">
-                        <nav id="sidebar" className={'ms-3'}>
-                            <div className="sidebar-header ">
-                                <h5>Main Menu</h5>
-                            </div>
-
-                            <p className={'cursor-pointer allCategories'}
-                               onClick={() => props.store.getDisplayedProducts()}>All
-                                Categories</p>
-                            <hr/>
-                            <ul className={'list-unstyled components'}>
-                                {categories ? categories.map((category: any) => {
-                                        return (<li className={'list-unstyled cursor-pointer categories'} key={category}
-                                                    onClick={() => handleCategorySelection(category)}>
-                                            {category}
-                                            <hr/>
-                                        </li>)
-                                    })
-                                    : <p>Categories Not Loaded</p>
-
-                                }
-                            </ul>
-                        </nav>
-                    </div>
-                </nav>
-                <main>
-                    <div className="p-3 my-2 rounded"
-                         style={{position: "absolute", top: '6rem', right: 0, zIndex: 9999}}>
-                        <Toast className={'bg-white'} isOpen={props.store.toastStatus}>
-                            <ToastHeader>
-                                Cart Updated
-                            </ToastHeader>
-                            <ToastBody>
-                                {props.store.cartUpdates.msg}
-                            </ToastBody>
-                        </Toast>
-                    </div>
-                    <Outlet context={data}/>
-                </main>
-            </section>
-
-
+            <CardGroup>
+                {data?.map((item: any) => {
+                    return (
+                        <Card key={item.id}>
+                            <CardImg
+                                alt="Card image cap"
+                                src={item.images[0]}
+                                top
+                                width={'200px'}
+                            />
+                            <CardBody>
+                                <CardTitle>
+                                    <strong>Product Name:</strong> {item.title}
+                                </CardTitle>
+                                <CardSubtitle>
+                                </CardSubtitle>
+                                <CardText>
+                                    <strong>Price:</strong>{item.price}
+                                </CardText>
+                                <CardText>
+                                    <strong>Category:</strong> {item.category}
+                                </CardText>
+                                <CardText>
+                                    <strong>Description:</strong> {item.description}
+                                </CardText>
+                                <Button>
+                                    Add To Cart
+                                </Button>
+                                <Button className='ml-5'>
+                                    Remove from cart
+                                </Button>
+                            </CardBody>
+                        </Card>
+                    )
+                })
+                }
+            </CardGroup>
         </>
     );
-})
+}
 export default Home;
